f1cda941d13f3538b2aed5bcd9034ee887ef47b3
Andreas Motl <andreas.motl@crate.io>
2023-08-25 19:51:17 +0200

    ETL: Improve section about Apache Kafka, add managed offerings

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,9 @@
 from crate.theme.rtd.conf.crate_clients_tools import *
+
+# TODO: Refactor into global configuration.
+linkcheck_ignore = [
+    "https://portal.azure.com/",
+    "https://azuremarketplace.microsoft.com/",
+]
+
+linkcheck_timeout = 5
