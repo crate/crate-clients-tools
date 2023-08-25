bbc8f63cbf0ba58be87859bfdb0720658a26f65c
Andreas Motl <andreas.motl@crate.io>
2023-08-25 22:05:13 +0200

    Disable version chooser, this project is not versioned

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,12 @@
 from crate.theme.rtd.conf.crate_clients_tools import *
 
+# Disable version chooser.
+html_context.update({
+    "display_version": False,
+    "current_version": None,
+    "versions": [],
+})
+
 # TODO: Refactor into global configuration.
 linkcheck_ignore = [
     "https://portal.azure.com/",
